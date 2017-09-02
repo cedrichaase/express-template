64a6251f8968e5cc6bdffd8f7424ba14ce3d5d48
Cedric Haase <cedric@sineband.de>
2017-09-02 19:21:25 +0200

    Do not ignore .js test files

---
+++ test/app/routes/user.js
@@ -0,0 +1,32 @@
+const container = require('../../../dist/out-tsc/src/app/container/container').default;
+const repo = container.resolve('userRepository');
+const request = require('request-promise');
+const assert = require('assert');
+
+describe('User routes', () => {
+
+    describe('GET /users', () => {
+        it('should return all users from the database', async () => {
+            const dbUsers = await repo.getUsers();
+
+            const response = await request('http://localhost:3000/users');
+            const apiUsers = JSON.parse(response).data;
+
+            assert.deepStrictEqual(apiUsers, dbUsers);
+        });
+    });
+
+    describe('GET /users/:id', () => {
+        it('should return the exact users from the database', async () => {
+
+            for (let id = 1; id < 100; id++) {
+                const dbUser = await repo.getUserById(id);
+
+                const response = await request(`http://localhost:3000/users/${id}`);
+                const apiUser = JSON.parse(response);
+
+                assert.deepStrictEqual(apiUser, dbUser);
+            }
+        });
+    });
+});
